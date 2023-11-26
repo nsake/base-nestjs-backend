001ad0a3b680c649dc6831b69434c0eb8b3b0f28
mnsycc <nikita.miniaylo.freeua@gmail.com>
2023-11-26 14:09:23 +0200

    file decorator to seize file from request

--- src/infrastructure/decorators/file-from-request.decorator.ts
+++ src/infrastructure/decorators/file-from-request.decorator.ts
@@ -1,21 +1,19 @@
 import {
-  ExecutionContext,
   BadRequestException,
+  ExecutionContext,
   createParamDecorator,
 } from '@nestjs/common';
 
 export const File = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext): Promise<File> => {
-    const request = ctx.switchToHttp().getRequest();
+    try {
+      const request = ctx.switchToHttp().getRequest();
 
-    // extract the files from the request object
-    const multiPart = await request.file();
+      const file = await request?.file();
 
-    if (!multiPart.file)
-      throw new BadRequestException(
-        'Invalid file or it is not exists in request',
-      );
-
-    return multiPart;
+      return file;
+    } catch (err) {
+      throw new BadRequestException('Can not proceed file parsing');
+    }
   },
 );
