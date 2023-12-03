87817ba3e11b45e7fb0601628ae07ccdf6247013
nsake <brukxkle@gmail.com>
2023-12-03 13:30:21 +0200

    add admin module

---
+++ src/modules/admin/admin.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateAdminDto } from './dto/update-admin.dto';
+
+@Injectable()
+export class AdminService {
+  findAll() {
+    return `This action returns all admin`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} admin`;
+  }
+
+  update(id: number, updateAdminDto: UpdateAdminDto) {
+    return `This action updates a #${id} admin`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} admin`;
+  }
+}
