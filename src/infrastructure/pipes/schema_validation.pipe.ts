0188b69cc9ec16f017612a8be63c4509b77432f0
nsake <brukxkle@gmail.com>
2023-12-05 16:28:03 +0200

    refactor schema_validation pipe; override global validation pipe; parse json on demand;

--- src/infrastructure/pipes/schema_validation.pipe.ts
+++ src/infrastructure/pipes/schema_validation.pipe.ts
@@ -1,23 +1,33 @@
-import {
-  PipeTransform,
-  Injectable,
-  BadRequestException,
-  Type,
-} from '@nestjs/common';
-import { validate } from 'class-validator';
+import { PipeTransform, Injectable, BadRequestException, Type } from '@nestjs/common';
+import { isJSON, validate } from 'class-validator';
 import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class SchemaValidationPipe<T> implements PipeTransform<any> {
-  constructor(private metatype: Type<any>) {}
+  constructor(private metatype: Type<any>, private propToTransform?: string[]) {}
 
   async transform(value: T): Promise<T> {
+    if (this.propToTransform) {
+      this.propToTransform.map((propertyName: string) => {
+        const entityToParse = value[propertyName];
+
+        if (!entityToParse) throw new BadRequestException('Can not find such query parameter');
+
+        if (!isJSON(entityToParse))
+          throw new BadRequestException(`${entityToParse} contains invalid JSON `);
+
+        value[propertyName] = JSON.parse(entityToParse);
+      });
+    }
+
     const object = plainToClass(this.metatype, value);
+
     Object.entries(object).forEach(([key, value]) => {
-      if (value === 'true' || value === 'false')
-        object[key] = value === 'true' ? true : false;
+      if (value === 'true' || value === 'false') object[key] = value === 'true' ? true : false;
     });
+
     const errors = await validate(object);
+
     if (errors.length > 0) {
       throw new BadRequestException({
         statusCode: 400,
@@ -26,6 +36,7 @@ export class SchemaValidationPipe<T> implements PipeTransform<any> {
         errors,
       });
     }
-    return value;
+
+    return object;
   }
 }
